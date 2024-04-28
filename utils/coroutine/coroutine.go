3db550645b87c9a82bfce873585f7c61933ff639
DSKY\toney.yin <toney.yin@idreamsky.com>
2024-04-28 15:36:24 +0800

    添加build time模块

---
+++ utils/coroutine/coroutine.go
@@ -0,0 +1,45 @@
+package coroutine
+
+import (
+	"fmt"
+	"github.com/hero1s/golib/log"
+	"reflect"
+	"runtime/debug"
+)
+
+func F(callback interface{}, recoverNum int, args ...interface{}) {
+	defer func() {
+		if r := recover(); r != nil {
+			var coreInfo string
+			coreInfo = string(debug.Stack())
+			coreInfo += "\n" + fmt.Sprintf("Core information is %v\n", r)
+			log.Errorf(coreInfo)
+			if recoverNum > 0 {
+				recoverNum -= 1
+			}
+			if recoverNum == -1 || recoverNum > 0 {
+				go F(callback, recoverNum, args...)
+			}
+		}
+	}()
+
+	v := reflect.ValueOf(callback)
+	if v.Kind() != reflect.Func {
+		panic("not a function")
+	}
+	vargs := make([]reflect.Value, len(args))
+	for i, arg := range args {
+		vargs[i] = reflect.ValueOf(arg)
+	}
+
+	v.Call(vargs)
+}
+
+func Go(callback interface{}, args ...interface{}) {
+	go F(callback, 0, args...)
+}
+
+// -1表示一直恢复
+func GoRecover(callback interface{}, recoverNum int, args ...interface{}) {
+	go F(callback, recoverNum, args...)
+}
