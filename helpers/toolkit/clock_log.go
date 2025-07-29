a72141e5c7a4fc10e3d1c7f57a352ec1b5212f60
DSKY\toney.yin <toney.yin@idreamsky.com>
2025-07-29 20:20:16 +0800

    Create clock_log.go

---
+++ helpers/toolkit/clock_log.go
@@ -0,0 +1,29 @@
+package toolkit
+
+import (
+	"github.com/hero1s/golib/log"
+	"time"
+)
+
+// 简单计时器
+type ClockLog struct {
+	name      string
+	startTime time.Time
+}
+
+// NewSimpleTimer 创建简单计时器
+func NewClockLog(name string) *ClockLog {
+	st := &ClockLog{
+		name:      name,
+		startTime: time.Now(),
+	}
+	log.Infof("[%s] 计时开始 at %v\n", st.name, st.startTime.Format("15:04:05.000"))
+	return st
+}
+
+// End 结束计时并输出日志
+func (st *ClockLog) End() {
+	endTime := time.Now()
+	duration := endTime.Sub(st.startTime)
+	log.Infof("[%s] 计时结束 at %v, 耗时: %v\n", st.name, endTime.Format("15:04:05.000"), duration)
+}
