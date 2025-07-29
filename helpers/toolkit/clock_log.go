f2855b7b47a5e1940ea63e086845b3c568795c5c
DSKY\toney.yin <toney.yin@idreamsky.com>
2025-07-29 20:26:37 +0800

    Update clock_log.go

--- helpers/toolkit/clock_log.go
+++ helpers/toolkit/clock_log.go
@@ -9,13 +9,15 @@ import (
 type ClockLog struct {
 	name      string
 	startTime time.Time
+	timeout   time.Duration
 }
 
 // NewSimpleTimer 创建简单计时器
-func NewClockLog(name string) *ClockLog {
+func NewClockLog(name string, timeout time.Duration) *ClockLog {
 	st := &ClockLog{
 		name:      name,
 		startTime: time.Now(),
+		timeout:   timeout,
 	}
 	log.Infof("[%s] 计时开始 at %v\n", st.name, st.startTime.Format("15:04:05.000"))
 	return st
@@ -25,5 +27,9 @@ func NewClockLog(name string) *ClockLog {
 func (st *ClockLog) End() {
 	endTime := time.Now()
 	duration := endTime.Sub(st.startTime)
-	log.Infof("[%s] 计时结束 at %v, 耗时: %v\n", st.name, endTime.Format("15:04:05.000"), duration)
+	if duration > st.timeout {
+		log.Errorf("[%s] 计时结束 at %v, 耗时: %v, 超时: %v\n", st.name, endTime.Format("15:04:05.000"), duration, st.timeout)
+	} else {
+		log.Infof("[%s] 计时结束 at %v, 耗时: %v\n", st.name, endTime.Format("15:04:05.000"), duration)
+	}
 }
